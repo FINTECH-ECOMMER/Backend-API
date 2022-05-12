21228c15a78edb14bf6089a36f60ad01e1b441f0
KenNguch <kennguch3@gmail.com>
2022-05-12 12:48:17 +0300

    Added: SpringBoot Initial Template To Use

---
+++ src/test/java/github/fintechecommerce/FintechEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.fintechecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechEcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
