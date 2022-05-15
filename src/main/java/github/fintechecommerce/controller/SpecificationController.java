7a62fab33cc3ca81a0471706090125c3f78f8761
KenNguch <kennguch3@gmail.com>
2022-05-15 11:31:27 +0300

    Fix: Routing in the Specification Controller

--- src/main/java/github/fintechecommerce/controller/SpecificationController.java
+++ src/main/java/github/fintechecommerce/controller/SpecificationController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 
+@RestController
+@RequestMapping(path = "${api_prefix}/specifications")
 public class SpecificationController {
 
     @Autowired
