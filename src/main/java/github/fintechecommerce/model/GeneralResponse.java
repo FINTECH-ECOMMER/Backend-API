c2be3a22d953d8c712124800f6899fb450613ebd
KenNguch <kennguch3@gmail.com>
2022-05-23 21:37:36 +0300

    Update:  Created all the routes required to run through error responses

--- src/main/java/github/fintechecommerce/model/GeneralResponse.java
+++ src/main/java/github/fintechecommerce/model/GeneralResponse.java
@@ -1,25 +1,19 @@
 package github.fintechecommerce.model;
 
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @RequiredArgsConstructor
 public class GeneralResponse {
+    private final Object data;
     private String status = HttpStatus.OK.getReasonPhrase();
     private int code = HttpStatus.OK.value();
-    @DateTimeFormat( pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime time = LocalDateTime.now();
-    private final Object data;
 }
