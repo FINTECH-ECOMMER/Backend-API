0edb5f6991fc011cc3a58bc8135dcf8749cab4cf
Burence1 <burensomondi@gmail.com>
2022-06-07 17:33:04 +0300

    Updated: errorhandling

--- src/main/java/github/fintechecommerce/model/ExemptionMessages.java
+++ src/main/java/github/fintechecommerce/model/ExemptionMessages.java
@@ -1,15 +1,16 @@
 package github.fintechecommerce.model;
 
 public enum ExemptionMessages {
-    IllegalArgumentException, JpaSystemException, NullPointerException, INTERNAL_SERVER_ERROR;
+    IllegalArgumentException, BAD_GATEWAY, NullPointerException, INTERNAL_SERVER_ERROR;
 
     public String getCustomMessage() {
 
         return switch (this) {
-            case IllegalArgumentException -> "small";
-            case JpaSystemException -> "ErrorLog in database";
-            case NullPointerException -> "NullPointerException";
+            case IllegalArgumentException -> "Conflict";
+            case BAD_GATEWAY -> "ErrorLog in database";
+            case NullPointerException -> "No Data Returned";
             case INTERNAL_SERVER_ERROR -> "Internal Server Error";
+
         };
     }
 }
