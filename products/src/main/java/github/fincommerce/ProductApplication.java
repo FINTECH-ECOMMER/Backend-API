47248d60203d8a0b28cf974f253b4a89c9cbb273
KenNguch <kennguch3@gmail.com>
2022-07-02 12:30:21 +0300

    Update: Added Service Discovery

--- products/src/main/java/github/fincommerce/ProductApplication.java
+++ products/src/main/java/github/fincommerce/ProductApplication.java
@@ -3,8 +3,10 @@ package github.fincommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableCaching
 public class ProductApplication {
 
