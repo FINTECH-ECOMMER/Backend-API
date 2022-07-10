dfa81e5007d2867890ceaf1b922ede60077aaf7b
Burence1 <burensomondi@gmail.com>
2022-07-10 14:23:09 +0300

    Added: Error service and error controller

---
+++ error/src/main/java/github/fintech/error/controller/ErrorController.java
@@ -0,0 +1,29 @@
+package github.fintech.error.controller;
+
+import github.fintech.error.Entity.Error;
+import github.fintech.error.model.ResponseModel;
+import github.fintech.error.service.ErrorService;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "${api_prefix}/errors")
+public class ErrorController {
+
+    @Autowired
+    private ErrorService errorService;
+
+    @GetMapping({"", "/"})
+    public ResponseModel<List<Error>> getErrors() {
+        return new ResponseModel<>(errorService.fetchAllErrors());
+    }
+
+    @PostMapping
+    public void postErrors(@RequestBody Error error){
+        errorService.postError(error);
+    }
+}
---
+++ error/src/main/java/github/fintech/error/service/ErrorService.java
@@ -0,0 +1,28 @@
+package github.fintech.error.service;
+
+import github.fintech.error.Entity.Error;
+import github.fintech.error.repository.ErrorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Service
+public class ErrorService {
+    public static final String KEY = "ERRORS";
+
+    @Autowired
+    private ErrorRepository errorRepository;
+
+    @Cacheable(value = KEY, unless = "#result == null")
+    public List<Error> fetchAllErrors() {
+        return errorRepository.findAll();
+    }
+
+    public Error postError(@RequestBody Error error) {
+        return errorRepository.save(error);
+    }
+}
