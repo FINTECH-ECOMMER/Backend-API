ce055f49894e3c541d7425426829c10a3da7e42c
KenNguch <kennguch3@gmail.com>
2022-07-12 11:33:07 +0300

    Added: Api Gateway

--- error/src/main/java/github/fintech/error/ErrorsApplication.java
+++ error/src/main/java/github/fintech/error/ErrorsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class ErrorsApplication {
 
     public static void main(String[] args) {
